98d7946c6e51de1679dca9edc335d222d313090e
nietaki <nietaki@gmail.com>
2016-03-13 14:58:22 +0000

    first successful reddit auth

--- app/io/github/nietaki/controllers/AuthController.scala
+++ app/io/github/nietaki/controllers/AuthController.scala
@@ -3,7 +3,8 @@ package io.github.nietaki.controllers
 import javax.inject.Inject
 
 import io.github.nietaki.modules.DatabaseConfigWrapper
-import play.api.mvc
+import play.api.{Logger, mvc}
+import play.api.libs.ws._
 import play.api.Play.current
 
 import scala.concurrent.Future
@@ -22,8 +23,22 @@ import io.github.nietaki.schemas._
 class AuthController @Inject() (dbConfigWrapper: DatabaseConfigWrapper) extends mvc.Controller {
 
   val db = dbConfigWrapper.dbConfig.db
+  val clientId = "FZpyk25KQW7MgA"
+  val secret = "w_hyUCUv61YKaN0Ndpptdu_APoY" // just temporarily for the dev version of the app
+  val redirectUri = "http://localhost:9000/auth/reddit_redirect" //same here
 
-  def redditRedirect(state: String, code: String) = mvc.Action {
-    Ok("redirect")
+  def redditRedirect(state: String, code: String) = mvc.Action.async {
+    for {
+      response <- getAccessToken(code, clientId, secret, redirectUri)
+    } yield Ok(response.body)
+  }
+  
+  def getAccessToken(code: String, clientId: String, clientSecret: String, redirectUri: String): Future[WSResponse] = {
+    val req = WS.url("https://www.reddit.com/api/v1/access_token")
+      .withAuth(clientId, clientSecret, WSAuthScheme.BASIC)
+      .withHeaders("Content-Type" -> "application/x-www-form-urlencoded")
+    req.post(Map("grant_type" -> Seq("authorization_code"), "code" -> Seq(code), "redirect_uri" -> Seq(redirectUri)))
+    //req.post(s"grant_type=authorization_code&code=$code&redirectUri=$redirectUri")
+    //req.post(s"grant_type=authorization_code")
   }
 }
