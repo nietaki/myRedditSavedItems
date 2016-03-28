5dc0cd8f3973900cd88939b1489c5a863f1e11df
nietaki <nietaki@gmail.com>
2016-03-28 19:29:21 +0100

    introduced bootstrap

--- public/javascripts/DisplayTable.js
+++ public/javascripts/DisplayTable.js
@@ -1,5 +1,36 @@
 window.DisplayTable = function() {
     var app = angular.module('savedItems', ["ngTable"]);
+
+    app.filter('ellipsis', function () {
+        return function (text, length) {
+            if(!text) {
+                return "";
+            }
+            if (text.length > length) {
+                return text.substr(0, length) + '...';
+            }
+            return text;
+        }
+    });
+    
+    app.filter('yesNo', function() {
+        return function(bool) {
+            if(!!bool) {
+                return "yes";
+            }
+            return "no";
+        }  
+    });
+    
+    app.filter('orDefault', function() {
+        return function(value, defaultValue) {
+            if(!!value) {
+                return value;
+            }
+            return defaultValue; 
+        }
+    });
+    
     app.controller("displayTableCtrl", function($scope, NgTableParams) {
         var self = this;
         var data = [{name: "Moroni", age: 50} /*,*/];
